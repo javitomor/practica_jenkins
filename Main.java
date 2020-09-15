0601234fcd0fc7ce3e21fbfaa51419b3c20a69d8
javitomor <javitomor@users.noreply.github.com>
2020-09-15 07:07:32 -0300

    Nuevos cambios

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main{
 	public static void main(String[] args){
 		System.out.println("Hola mundo, desde java");
+		System.out.println("La conexión con Github se ha realizado");
 }
 
 }
